3cdf06055586f2097754c0ef9c2103704c915dc4
stephjac000 <stephjac000@warren.k12.in.us>
2025-07-30 05:32:22 -0400

    Done did got this done!!!

---
+++ aboutMe.java
@@ -0,0 +1,12 @@
+public class aboutMe {
+    public static void main(String[] args) {
+        // declare your name, age, and one fun fact as a String, int, and another String
+        // print each on their own line.
+        String name = "Jacob";
+        int age = 16;
+        String funFact = "I like to code!!!";
+        System.out.println("My name is: " + name);
+        System.out.println("My age is: " + age);
+        System.out.println("My fun is: " + funFact);
+    }
+}
